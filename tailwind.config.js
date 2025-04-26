322ec6e5bda80abc116dae26e6646c0322ce80ac
Brian <thinhle1199@gmail.com>
2025-04-26 14:58:45 -0500

    adjust theme to look like aws

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,31 +7,38 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          DEFAULT: "#5a50e2",
-          50: "#f1f0fc",
-          100: "#e3e1fa",
-          200: "#c6c3f5",
-          300: "#aaa5ef",
-          400: "#8d87ea",
-          500: "#5a50e2",
-          600: "#3e34ca",
-          700: "#3329a1",
-          800: "#252077",
-          900: "#17154e",
+          DEFAULT: "#232F3E", // AWS dark blue/navy
+          50: "#E6EAEF",
+          100: "#CCD5DF",
+          200: "#99ABBF",
+          300: "#66819F",
+          400: "#33587F",
+          500: "#232F3E", // AWS dark blue/navy
+          600: "#1C2632",
+          700: "#151C25",
+          800: "#0F1319",
+          900: "#08090C",
         },
         secondary: {
-          DEFAULT: "#ff8a00",
-          50: "#fff5e6",
-          100: "#ffebcc",
-          200: "#ffd699",
-          300: "#ffc266",
-          400: "#ffad33",
-          500: "#ff8a00",
-          600: "#cc6f00",
-          700: "#995200",
-          800: "#663600",
-          900: "#331b00",
+          DEFAULT: "#FF9900", // AWS primary orange
+          50: "#FFF4E6",
+          100: "#FFE8CC",
+          200: "#FFD699",
+          300: "#FFC266",
+          400: "#FFAD33",
+          500: "#FF9900", // Main AWS orange
+          600: "#EC7211", // Alternate AWS orange (smile)
+          700: "#CC7A00",
+          800: "#995C00",
+          900: "#331F00",
         },
+        aws: {
+          orange: "#FF9900",
+          navy: "#232F3E",
+          squid: "#31465F", // Darker blue used in certain elements
+          anchor: "#00A1C9", // Teal blue for links/buttons
+          smile: "#EC7211", // Alternate orange
+        }
       },
     },
   },
